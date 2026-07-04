1e0689d9ce2e0e5f4764a9a558b1179086d4c444
Addy056 <aiaera056@gmail.com>
2026-07-04 16:16:59 +0530

    pages updated

--- services/groq.service.js
+++ services/groq.service.js
@@ -13,7 +13,7 @@ export const generateAIReply = async (prompt) => {
     const completion =
       await groq.chat.completions.create({
 
-        model: "llama3-70b-8192",
+        model: "llama-3.1-8b-instant",
 
         temperature: 0.7,
 
